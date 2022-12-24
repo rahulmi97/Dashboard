71f35036fdcb5ed18eb602a1084a38e8881229b6
rahul.mishra <rahul.mishra@centilytics.com>
2022-12-24 20:35:20 +0530

    dashboard page created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'job-portal'`, () => {
+  it(`should have as title 'dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('job-portal');
+    expect(app.title).toEqual('dashboard');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'job-portal app is running!'
+      'dashboard app is running!'
     );
   });
 });
